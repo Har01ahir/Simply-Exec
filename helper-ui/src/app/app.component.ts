9eef18a383cd3b7a1f525ef54d2e3b4cd1daf5b5
Har01ahir <hardikladumor01@gmail.com>
2023-06-24 18:47:57 +0530

    Added after merge with dev

--- helper-ui/src/app/app.component.ts
+++ helper-ui/src/app/app.component.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helper';
-<<<<<<< HEAD
-=======
-  Welcome = false;
->>>>>>> dev
 }
--- helper-ui/src/app/contract/view-contracts/view-contracts.component.ts
+++ helper-ui/src/app/contract/view-contracts/view-contracts.component.ts
@@ -2,11 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Contract } from '../contract.model';
-<<<<<<< HEAD
-import { map } from 'rxjs';
-=======
 import { Observable, map } from 'rxjs';
->>>>>>> dev
 ;
 
 
@@ -17,12 +13,8 @@ import { Observable, map } from 'rxjs';
 })
 export class ViewContractsComponent implements OnInit, OnDestroy {
   dtOptions: DataTables.Settings = {};
-<<<<<<< HEAD
-  contracts!: Contract[];
-=======
   contracts!: Observable<Contract[]>;
 
->>>>>>> dev
   // table = new DataTable('#myTable');
 
   constructor(
@@ -34,29 +26,11 @@ export class ViewContractsComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
-<<<<<<< HEAD
-    const that = this
-=======
     // const that = this
->>>>>>> dev
-    // this.fetchContracts();
+    this.fetchContracts();
 
     // $('#myTable').append();
 
-<<<<<<< HEAD
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 5,
-      processing: true,
-      ajax: (dataTablesParameters: any, callback) => {
-        that.http.get<Contract[]>('http://localhost:3000/contract/view-contracts').subscribe(resp => {
-          that.contracts = resp;
-          console.log(resp);
-        });
-      },
-      columns: [{ data: 'id' }, { data: 'customerId' }, { data: 'vendorId' }, { data: 'customer_payment_status' }, { data: 'vendor_delivery_status' }, { data: 'status' }, { data: 'created_at' }, { data: 'updated_at'} ]
-    };
-=======
     // this.dtOptions = {
     //   pagingType: 'full_numbers',
     //   pageLength: 5,
@@ -69,18 +43,13 @@ export class ViewContractsComponent implements OnInit, OnDestroy {
     //   },
     //   columns: [{ data: 'id' }, { data: 'customerId' }, { data: 'vendorId' }, { data: 'customer_payment_status' }, { data: 'vendor_delivery_status' }, { data: 'status' }, { data: 'created_at' }, { data: 'updated_at'} ]
     // };
->>>>>>> dev
   }
 
   
 
   // Get all Contracts from server
   fetchContracts() {
-<<<<<<< HEAD
-    this.http.
-=======
     this.contracts = this.http.
->>>>>>> dev
       get<{ [key: string]: Contract }>('http://localhost:3000/contract/view-contracts')
       .pipe(map((responseData) => { // operator observable
         let contracts: Contract[] = []
@@ -90,15 +59,9 @@ export class ViewContractsComponent implements OnInit, OnDestroy {
         console.log(contracts);
         return contracts;
       }))
-<<<<<<< HEAD
-      .subscribe((response: Contract[]) => { // subscribe
-        this.contracts = response;
-      });
-=======
       // .subscribe((response: Contract[]) => { // subscribe
       //   this.contracts = response;
       // });
->>>>>>> dev
   }
 
 
