68a4b98f9d39271e3ec780461a61db1de45a3179
Har01ahir <hardikladumor01@gmail.com>
2023-06-28 10:30:06 +0530

    Changes updated

--- helper/src/app.controller.ts
+++ helper/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/contract/create-contract')
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Delete('/:id')
+  deleteRecord(@Param('id', ParseIntPipe) id: number) {
+    return "record Deleted Successfully with id : "+id;
   }
 }
